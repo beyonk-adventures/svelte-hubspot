e541d3b2f0f03a9723153b56a5fe605cd4e5293e
Alex Dilley <alex.dilley@gmail.com>
2021-10-21 11:33:12 +0100

    use of hs cookie to restrict tracking based on consent

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,7 @@
   bind:this={hs}
   subdomain="js-eu1"
   hubId={import.meta.env.VITE_HUB_ID}
-  disabled={true}
+  disabled={!consented}
 />
 
 <main>
@@ -12,16 +12,28 @@
 <footer>
   {#if !consented}
     <button type="button" on:click={consent}>Consent</button>
+  {:else}
+    <button type="button" on:click={revoke}>Revoke</button>
   {/if}
 </footer>
 
 <script>
   import { onMount } from 'svelte'
+  import { browser } from '$app/env'
   import { navigating, page } from '$app/stores'
   import { HubSpotTracking } from '$lib'
 
+  const cookie = '__hs_do_not_track'
+
   let hs
-  let consented = false
+  let consented =
+    browser &&
+    document.cookie
+      .split('; ')
+      .reduce((acc, c) => {
+        const [name, value] = c.split('=')
+        return { ...acc, [name]: value }
+      }, {})[cookie] === 'no'
 
   $: if ($navigating) {
     hs.setPath($navigating.to)
@@ -34,6 +46,12 @@
 
   function consent () {
     consented = true
+    document.cookie = `${cookie}=no;max-age=${60*60*24*365}`
     hs.init()
+  }
+
+  function revoke () {
+    document.cookie = `${cookie}=yes;max-age=${60*60*24*365}`
+    consented = false;
   }
 </script>
